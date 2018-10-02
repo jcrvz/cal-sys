42250d71311ecf117665ec5b0d45b42a5311f11f
Jorge Mario Cruz-Duarte <mrcrois@gmail.com>
2018-10-02 10:44:52 -0500

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -2,8 +2,8 @@
 
 echo 'Launcher was launched!'
 echo '---------------------------------------------'
-/usr/bin/python ~/rolac_v3.py &
+/usr/bin/python ~/rolac.py &
 echo '---> Measurement script is running as ' $!
-/usr/bin/octave-cli ~/quickplot_v5.m &
+/usr/bin/octave-cli ~/quickplot.m &
 echo '---> Plot script is running as ' $! 
 echo '---------------------------------------------'
